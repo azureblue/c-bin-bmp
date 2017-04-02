5da9e1440154c34f827bf8fc922adba08b5ed213
azureblue <kk0xf0@gmail.com>
2017-04-02 02:10:55 +0200

    removing consts from int params in header

--- bmp.h
+++ bmp.h
@@ -1,6 +1,6 @@
 #ifndef BMP_H
 #define BMP_H
 
-int writeBinBmp(const char * filePath, const int w, const int h, const int * pixels);
+int writeBinBmp(const char * filePath, int w, int h, const int * pixels);
 
 #endif /* BMP_H */
